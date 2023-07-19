0969dec399d4cab0a7f9a02d8e5962ce96271211
Mikhail Grechka <mishagreh@gmail.com>
2023-07-19 12:37:36 +0600

    add html tags

---
+++ _html_tags.py
@@ -0,0 +1,19 @@
+# 15. Write a Python function to create an HTML string with tags around the word(s).
+# Sample function and result :
+# add_tags('i', 'Python') -> '<i>Python</i>'
+# add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+
+def html_tags(tag, word: str) -> str:
+    return f'<{tag}>{word} </{tag}>'
+
+
+tag, word = 'b', 'Python Tutorial'
+print(html_tags(tag, word))
+
+
+# solution
+#
+# def add_tags(tag, word):
+# 	return "<%s>%s</%s>" % (tag, word, tag)
+# print(add_tags('i', 'Python'))
+# print(add_tags('b', 'Python Tutorial'))
