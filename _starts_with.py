f73a45b228fce01fc352c64002c72b832261cfa4
Mikhail Grechka <mishagreh@gmail.com>
2023-07-21 16:30:47 +0600

    starts with

---
+++ _starts_with.py
@@ -0,0 +1,13 @@
+# 24. Write a Python program to check whether a string starts with specified characters.
+
+def starts_with(s: str) -> bool:
+    return s.startswith('x')
+
+
+s = 'qweqweqwe'
+print(starts_with(s))
+
+# solution
+#
+# string = "w3resource.com"
+# print(string.startswith("w3r"))
